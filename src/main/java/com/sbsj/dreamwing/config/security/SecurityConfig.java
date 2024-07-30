2155ea5c5a77f9bfd60f73c22400c5d718c008b1
Eunchan Jeong <jec65147@gmail.com>
2024-07-31 01:12:41 +0900

    Feat : 회원탈퇴 기능 구현

--- src/main/java/com/sbsj/dreamwing/config/security/SecurityConfig.java
+++ src/main/java/com/sbsj/dreamwing/config/security/SecurityConfig.java
@@ -29,30 +29,21 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        //  기본 설정 사용 안함
-        http.httpBasic(httpBasic ->httpBasic.disable());
-
-        // 사이트 위변조 요청 방지 (csrf 비활성화)
-        http.csrf(csrf -> csrf.disable());
-
-
         http
-                .authorizeRequests((authorizeRequests) -> authorizeRequests
-                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll());
-//        http
-//                .httpBasic(httpBasic -> httpBasic.disable()) // 기본 인증 비활성화
-//                .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 관리 없음
-//                .authorizeHttpRequests(authorize -> authorize
+                .httpBasic(httpBasic -> httpBasic.disable()) // 기본 인증 비활성화
+                .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 관리 없음
+                .authorizeHttpRequests(authorize -> authorize
 //                        .requestMatchers("/user/withdraw").hasRole("USER") // /user/withdraw 경로는 USER 역할을 가진 사용자만 접근 가능
 //                        .requestMatchers("/user/login", "/user/join").permitAll() // /login 및 /join 경로는 인증 없이 접근 가능
-//                        .anyRequest().authenticated() // 나머지 모든 경로는 인증 필요
-//                )
-//                .exceptionHandling(exceptions -> exceptions
-//                        .accessDeniedHandler(customAccessDeniedHandler)
-//                        .authenticationEntryPoint(customAuthenticationEntryPoint)
-//                )
-//                .addFilterBefore(new JwtFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class); // JWT 필터 추가
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
+                        .anyRequest().authenticated() // 나머지 모든 경로는 인증 필요
+                )
+                .exceptionHandling(exceptions -> exceptions
+                        .accessDeniedHandler(customAccessDeniedHandler)
+                        .authenticationEntryPoint(customAuthenticationEntryPoint)
+                )
+                .addFilterBefore(new JwtFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class); // JWT 필터 추가
 
         return http.build();
     }
