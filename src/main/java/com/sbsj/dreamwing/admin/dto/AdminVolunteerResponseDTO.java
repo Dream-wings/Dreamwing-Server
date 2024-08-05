65038f245bdbd17ecc6d48f48ffe3fa119e05112
JAESEONGIM <bigstar2878@gmail.com>
2024-08-05 12:23:25 +0900

    Feat : 관리자 페이지 봉사 리스트 조회

--- src/main/java/com/sbsj/dreamwing/admin/dto/AdminVolunteerResponseDTO.java
+++ src/main/java/com/sbsj/dreamwing/admin/dto/AdminVolunteerResponseDTO.java
@@ -5,7 +5,8 @@ import lombok.Data;
 @Data
 public class AdminVolunteerResponseDTO {
     private long volunteerId;
-    private String title;                         // 봉사 or 멘토링 제목
-    private int type;                             // 봉사 or 멘토링
-    private int currentPerson;                    // 현재 인원 수 (실제 컬럼이 아니며 쿼리로 계산)
+    private int type;
+    private String title;
+    private int currentCount;
+    private int totalCount;               // 현재 인원 수 (실제 컬럼이 아니며 쿼리로 계산)
 }
